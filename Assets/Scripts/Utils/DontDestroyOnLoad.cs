012c20bb2ef9f75c2a30675674bc05cd3ec7f376
Yonatansheffer <yonatan.sheffer@mail.huji.ac.il>
2025-05-30 10:34:33 +0300

    Added Sound Scripts

---
+++ Assets/Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
