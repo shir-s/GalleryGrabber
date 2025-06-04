216b4c5c45ef19211a7e388fa0748e9254b7cea3
Yonatansheffer <yonatan.sheffer@mail.huji.ac.il>
2025-06-04 19:53:22 +0300

    Fixed canvas bug

---
+++ Assets/Scripts/Managers/PointManager.cs
@@ -0,0 +1,16 @@
+﻿using Unity.VisualScripting;
+using Utils;
+
+namespace Managers
+{
+    public class PointManager
+    {
+        private int _points;
+
+        private void OnEnable()
+        {
+            
+        }
+
+    }
+}
