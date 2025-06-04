c9c43bbc2bc64b73d089b89f236303f4fc76313b
edenavrahami <eden.avrahami@mail.huji.ac.il>
2025-06-04 14:40:52 +0300

    player health

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Utils;
+
+namespace Player
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        public int maxHealth = 3;
+        private int currentHealth;
+
+        private void Start()
+        {
+            currentHealth = maxHealth;
+            GameEvents.PlayerHealthChanged?.Invoke(currentHealth, maxHealth);
+        }
+
+        public void TakeDamage(int amount)
+        {
+            currentHealth -= amount;
+            GameEvents.PlayerHealthChanged?.Invoke(currentHealth, maxHealth);
+
+            if (currentHealth <= 0)
+            {
+                GameEvents.GameOver?.Invoke();
+            }
+        }
+    }
+
+}
