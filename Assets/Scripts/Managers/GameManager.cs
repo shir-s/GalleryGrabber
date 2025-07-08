390b6782ea9e7a93da0cbc9c80eec843acb16087
Yonatansheffer <yonatan.sheffer@mail.huji.ac.il>
2025-07-08 22:05:19 +0300

    configure

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ namespace Managers
     public static class GameManager
     {
         public static GameOverReason LastGameOverReason { get; set; } 
-        public static int MaxDirt { get; set; } = 40;
+        public static int MaxDirt { get; set; } = 20;
         public static bool isPlayerCaught { get; set; } = false;
     }
 
